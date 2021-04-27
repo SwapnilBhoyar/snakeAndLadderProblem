a26a5b6fff4458bf759d7ff36c0298ecd334e34d
SwapnilBhoyar <swapnilbhoyar95@gmail.com>
2021-04-27 20:18:39 +0530

    added die's

--- snakeAndLadder.java
+++ snakeAndLadder.java
@@ -2,6 +2,8 @@ public class snakeAndLadder {
 	public static void main(String args[]) {
 		int position = 0;
 		System.out.println("Current Position is:" + position);
+		int die = (int)Math.floor( ( ( Math.random() * 10 ) % 6) + 1);
+		System.out.println("Number on die is:" + die);
 	}
 
 }
